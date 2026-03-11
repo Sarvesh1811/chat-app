0f57a41660046c27d4c321ca8715417b85379559
Sarvesh1811 <pawarsarvesh1811@gmail.com>
2026-03-11 23:29:00 +0530

    fix cors

--- server/index.js
+++ server/index.js
@@ -6,9 +6,6 @@ const router = require('./routes/index')
 const cookiesParser = require('cookie-parser')
 const { app, server } = require('./socket/index')
 
-// const app = express()
-const cors = require("cors")
-
 app.use(cors({
   origin: [
     "http://localhost:3000",
@@ -16,22 +13,22 @@ app.use(cors({
   ],
   credentials: true
 }))
+
 app.use(express.json())
 app.use(cookiesParser())
 
 const PORT = process.env.PORT || 8080
 
-app.get('/',(request,response)=>{
-    response.json({
-        message : "Server running at " + PORT
-    })
+app.get('/',(req,res)=>{
+  res.json({
+    message: "Server running at " + PORT
+  })
 })
 
-//api endpoints
-app.use('/api',router)
+app.use('/api', router)
 
 connectDB().then(()=>{
-    server.listen(PORT,()=>{
-        console.log("server running at " + PORT)
-    })
+  server.listen(PORT,()=>{
+    console.log("server running at " + PORT)
+  })
 })
